80c245618e98142ea787cefadedc99fd1fcee8fb
priyanshu2304 <priyanshumodi1998@gmail.com>
2023-02-09 19:25:26 +0530

    Pie Chart Component and Sip Calculator

---
+++ components/Common/Charts.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div :id="chartId"></div>
+  </div>
+</template>
+
+<script>
+import Highcharts from 'highcharts'
+export default {
+  name: 'PieChart',
+  props: ['chartId', 'title', 'seriesData'],
+  mounted() {
+    console.log('seriesData', this.seriesData)
+    Highcharts.chart(this.chartId, {
+      chart: {
+        type: 'pie',
+      },
+      title: {
+        text: this.title,
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          },
+        },
+      },
+      series: [
+        {
+          name: this.title,
+          data: this.seriesData,
+        },
+      ],
+    })
+  },
+}
+</script>
--- pages/calculator/sip-calculator.vue
+++ pages/calculator/sip-calculator.vue
@@ -11,48 +11,97 @@
             type="number"
           />
         </div>
-        <div class="mf-input__section">
-          <label class="mf-input__heading">No Of Years Invested</label>
-          <input class="mf-input__value" v-model="noOfYear" type="number" />
-        </div>
-        <div class="mf-input__section">
-          <label class="mf-input__heading">Annual rate Of Interest</label>
-          <input
-            class="mf-input__value"
-            v-model="rateOfInterest"
-            type="number"
-          />
-        </div>
-        <button class="rm-btn__black width" @click="submit">Calculator</button>
-        <div class="mf-calculator__display-section">
+        <form @submit.prevent="submit">
+          <div class="mf-input__section">
+            <label class="mf-input__heading">No Of Years Invested</label>
+            <input class="mf-input__value" v-model="noOfYear" type="number" />
+          </div>
+          <div class="mf-input__section">
+            <label class="mf-input__heading">Annual rate Of Interest</label>
+            <input
+              class="mf-input__value"
+              v-model="rateOfInterest"
+              type="number"
+            />
+          </div>
+          <button class="rm-btn__black width" type="submit">Calculator</button>
+        </form>
+        <div v-if="amount" class="mf-calculator__display-section">
           <div class="mf-calculator__display-heading">Total Amount</div>
           <div class="mf-calculator__display-value">
-            ₹{{ Amount && Amount.toFixed(2) }}
+            ₹{{ amount && formatNumberWithComma(amount) }}
+          </div>
+        </div>
+        <div v-if="investedAmount" class="mf-calculator__display-section">
+          <div class="mf-calculator__display-heading">
+            Total Invested Amount
+          </div>
+          <div class="mf-calculator__display-value">
+            ₹{{ investedAmount && formatNumberWithComma(investedAmount) }}
+          </div>
+        </div>
+        <div v-if="earnedMoney" class="mf-calculator__display-section">
+          <div class="mf-calculator__display-heading">Total Earned Amount</div>
+          <div class="mf-calculator__display-value">
+            ₹{{ earnedMoney && formatNumberWithComma(earnedMoney) }}
           </div>
         </div>
       </div>
-      <div class="mf-calculator__chart-section">hy</div>
+      <div
+        v-if="investedAmount && earnedMoney && amount"
+        class="mf-calculator__chart-section"
+      >
+        <chart-component
+          :key="amount"
+          chartId="pie-chart"
+          title="Sip Calculator"
+          :seriesData="[
+            {
+              name: 'Invested Money',
+              y: Number(investedAmount),
+            },
+            {
+              name: 'Earned Money',
+              y: Number(earnedMoney),
+              sliced: true,
+              selected: true,
+            },
+          ]"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ChartComponent from '@/components/Common/Charts.vue'
 export default {
   data() {
     return {
       monthlyAmount: null,
       noOfYear: null,
       rateOfInterest: null,
-      Amount: null,
+      amount: null,
+      investedAmount: null,
+      earnedMoney: null,
     }
+  },
+  components: {
+    ChartComponent,
   },
   methods: {
     submit() {
-      this.Amount =
+      this.amount = (
         (this.monthlyAmount *
           (Math.pow(1 + this.rateOfInterest / 100 / 12, 12 * this.noOfYear) -
             1)) /
         (this.rateOfInterest / 100 / 12)
+      ).toFixed(2)
+      this.investedAmount = (this.monthlyAmount * 12 * this.noOfYear).toFixed(2)
+      this.earnedMoney = (this.amount - this.investedAmount).toFixed(2)
+    },
+    formatNumberWithComma(number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
     },
   },
 }
